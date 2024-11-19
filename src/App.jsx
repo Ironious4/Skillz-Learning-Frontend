30f9892b95bb07e8f816ee3cf058a9df09c05f7c
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-11-19 23:17:18 +0300

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-
+import Profile from './pages/InstructorProfile';
+import InstructorProfile from './components/InstructorProfile'; 
+import UsersList from './pages/users';
 
 const App = () => {
   return (
@@ -10,6 +12,9 @@ const App = () => {
           <div className="content">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path='/users' element={<UsersList/>}/>
+              <Route path='/profile' element={<Profile/>}/>
+              <Route path='/instructorprofile' element={<InstructorProfile/>}/>
             </Routes> 
           </div>
         </Router>
