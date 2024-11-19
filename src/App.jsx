f622c9213a4aeb1f9adb47632e79d3c149ba4b67
sheriff <chege2452@gmail.com>
2024-11-20 00:58:30 +0300

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignUpForm from './components/SignUpForm';
-import SignInForm from './components/SignInForm';
 import Home from './pages/Home';
-import Profile from './pages/InstructorProfile';
-import InstructorProfile from './components/InstructorProfile'; 
-import UsersList from './pages/users';
-import PaymentPage from './pages/PaymentPage';
-import Browser from './pages/Browser';
-import { MessagesProvider } from './components/MessagesContext'; 
-import MessagesPage from './pages/MessagesPage';
-import TwoFactorAuth from './components/TwoFactorAuth'
-
 const App = () => {
   return (
     <MessagesProvider> 
@@ -19,15 +8,6 @@ const App = () => {
           <div className="content">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/signin" element={<SignInForm />} />
-              <Route path="/signup" element={<SignUpForm />} />
-              <Route path="/users" element={<UsersList />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/instructorprofile" element={<InstructorProfile />} />
-              <Route path="/messages" element={<MessagesPage />} />
-              <Route path="/payment/:courseId/:learnerId/:price" element={<PaymentPage />} />
-              <Route path="/browser" element={<Browser />} />
-              <Route path="/TwoFactorAuth" element={<TwoFactorAuth />} />
             </Routes> 
           </div>
         </Router>
